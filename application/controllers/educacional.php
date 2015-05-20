4251518334a752d22a3d5a2fec62ac2243682b3f
iaraujoisraell <iaraujo.israel@gmail.com>
2015-05-20 13:47:57 -0400

    Exclusao da pasta curso
    eu tinha criado uma pasta curso dentro da view, mas depois exclui

--- application/controllers/educacional.php
+++ application/controllers/educacional.php
@@ -86,5 +86,56 @@ class educacional extends CI_Controller {
         $this->load->view('../views/educacional/index', $page_data);
     }
 
+     function cursos($param1 = '', $param2 = '', $param3 = '') {
+        if ($this->session->userdata('admin_login') != 1)
+            redirect(base_url(), 'refresh');
+        if ($param1 == 'create') {
+            $data['name'] = $this->input->post('name');
+            $data['birthday'] = $this->input->post('birthday');
+            $data['sex'] = $this->input->post('sex');
+            $data['address'] = $this->input->post('address');
+            $data['phone'] = $this->input->post('phone');
+            $data['email'] = $this->input->post('email');
+            $data['password'] = $this->input->post('password');
+            $this->db->insert('teacher', $data);
+            $teacher_id = mysql_insert_id();
+            move_uploaded_file($_FILES['userfile']['tmp_name'], 'uploads/teacher_image/' . $teacher_id . '.jpg');
+            $this->email_model->account_opening_email('teacher', $data['email']); //SEND EMAIL ACCOUNT OPENING EMAIL
+            redirect(base_url() . 'index.php?admin/teacher/', 'refresh');
+        }
+        if ($param1 == 'do_update') {
+            $data['name'] = $this->input->post('name');
+            $data['birthday'] = $this->input->post('birthday');
+            $data['sex'] = $this->input->post('sex');
+            $data['address'] = $this->input->post('address');
+            $data['phone'] = $this->input->post('phone');
+            $data['email'] = $this->input->post('email');
+            $data['password'] = $this->input->post('password');
+
+            $this->db->where('teacher_id', $param2);
+            $this->db->update('teacher', $data);
+            move_uploaded_file($_FILES['userfile']['tmp_name'], 'uploads/teacher_image/' . $param2 . '.jpg');
+            redirect(base_url() . 'index.php?admin/teacher/', 'refresh');
+        } else if ($param1 == 'personal_profile') {
+            $page_data['personal_profile'] = true;
+            $page_data['current_teacher_id'] = $param2;
+        } else if ($param1 == 'edit') {
+            $page_data['edit_data'] = $this->db->get_where('teacher', array(
+                        'teacher_id' => $param2
+                    ))->result_array();
+        }
+        if ($param1 == 'delete') {
+            $this->db->where('teacher_id', $param2);
+            $this->db->delete('teacher');
+            redirect(base_url() . 'index.php?admin/teacher/', 'refresh');
+        }
+
+        $page_data['cursos'] = $this->db->get('cursos')->result_array();
+        //SELECT ABAIXO PARA MONTAR O MENU ACESSO, DEVE SER INCLUIDO EM TODOS OS MENUS
+        $page_data['acesso'] = $this->db->get('acessos')->result_array();
+        $page_data['page_name'] = 'Cursos';
+        $page_data['page_title'] = get_phrase('<a href="index.php?admin/dashboard">Painel Geral</a> > <a href="index.php?admin/educacional">Painel_educacional </a><b>></b> <a href="">Gerenciar_cursos</a>');
+        $this->load->view('../views/educacional/index', $page_data);
+    }
 }
 ?>
